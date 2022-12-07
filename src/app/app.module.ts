0d4a14c17c2cbb949ed093a9e89007b6b8649439
“milton.cifuentes” <milton.cifuentes@bancamia.com.co>
2022-12-06 19:04:57 -0500

    Creación manual de carpeta books, archivos books.component.ts,books.component.html,books.component.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Bookscomponent } from './books/books.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Bookscomponent,
+    BookComponent
   ],
   imports: [
     BrowserModule
